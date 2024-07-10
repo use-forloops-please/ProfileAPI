131f1d6146277e20ea3476ce21ffdd12899bd72f
lukejansevanrensburg <luke1time18@gmail.com>
2024-07-10 19:08:23 +0200

    removed unessary code

--- NewProfileAPI/Properties/Program.cs
+++ NewProfileAPI/Properties/Program.cs
@@ -3,7 +3,6 @@ using ProfileAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
